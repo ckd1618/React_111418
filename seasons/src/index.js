bbb47ae8f940c07960f2296062fb29a50669b64e
ckd1618 <ckd1618@gmail.com>
2018-11-18 21:52:33 -0800

    can now display the latitude on screen, or an error message if it failed

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+
     //class based components use render() { } while functional do not
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-        return <div>Latitude: </div>;
+    //the render is basically you adding a prototype method from es5, or it works the same way as this.render= function() in constructor
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: { this.state.errorMessage }</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: { this.state.lat }</div>;
+        }
+        return <div>Loading...</div>;
     }
 }
 
