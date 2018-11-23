e3f2ce4a14861a87d591b00bb24ab10bab81bcb0
ckd1618 <ckd1618@gmail.com>
2018-11-22 23:39:21 -0800

    update

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
 
-        this.state = { lat: null, errorMessage: '' };
+    //     this.state = { lat: null, errorMessage: '' };
+    // }
 
+    state = { lat: null, errorMessage: '' }
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
 
