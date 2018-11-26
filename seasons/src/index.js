b016f84ca2835dc377bd77bd35bc76fd3543f9f6
ckd1618 <ckd1618@gmail.com>
2018-11-25 23:52:06 -0800

    added comments

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 
     state = { lat: null, errorMessage: '' }
 
+    // this component triggers right after the constructor i.e. state
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => this.setState({ lat: position.coords.latitude }),
